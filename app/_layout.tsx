8b6594e91ed3f4fa411b03ded174fa6623a31318
barnettrob <barnett.rob@gmail.com>
2024-05-11 17:39:44 -0400

    add tabs that will replace header config popup

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,47 @@
-import { Stack } from "expo-router";
+import { Stack, Tabs } from "expo-router";
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useColorScheme } from '@/hooks/useColorScheme';
+import { TabBarIcon } from '@/components/navigation/TabBarIcon';
+import { Colors } from '@/constants/Colors';
+import { Ionicons } from '@expo/vector-icons';
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   return (
+    // <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    //   <Stack>
+    //     <Stack.Screen name="index" options={{ headerShown: false }} />
+    //   </Stack>
+    // </ThemeProvider>
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="index" options={{ headerShown: false }} />
-      </Stack>
-    </ThemeProvider>
+      <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        headerShown: false,
+      }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Home',
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="settings"
+        options={{
+          title: 'Config',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons
+              name="settings-outline"
+              color={color}
+              size={size}
+            />
+          ),
+        }}
+      />
+    </Tabs>
+  </ThemeProvider>
   );
 }
--- app/settings.tsx
+++ app/settings.tsx
@@ -1,8 +1,14 @@
 import { Text } from "react-native";
+import { useColorScheme } from '@/hooks/useColorScheme';
+
 const settings = () => {
   return (
-    <Text>
+    <Text style={{ color: '#FFF', marginTop: 100}}>
       Settings
+
+      dfasdfsd
+      dsdfdss
+      adfsdfd
     </Text>
   )
 }
