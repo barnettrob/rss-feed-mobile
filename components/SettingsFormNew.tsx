04afbd8d0c369b904f78a3d1e1c518c7f20f52ff
barnettrob <barnett.rob@gmail.com>
2024-05-15 12:08:19 -0400

    working out asyncStorage and settings form

--- components/SettingsFormNew.tsx
+++ components/SettingsFormNew.tsx
@@ -1,6 +1,5 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { 
-  Button,
   Pressable, 
   Text, 
   TextInput, 
@@ -10,10 +9,12 @@ import {
 import { useColorScheme } from '@/hooks/useColorScheme';
 import { useForm, Controller } from 'react-hook-form';
 import AntDesign from '@expo/vector-icons/AntDesign';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const SettingsFormNew = () => {
   const [inputNum, setInputNum] = useState([1]);
   const [submittedData, setSubmittedData] = useState([]);
+  const [feedDataStorage, setFeedDataStorage] = useState({});
   const inputsLength = inputNum.length;
   const inputRef = useRef(new Array(inputsLength));
   const { control, handleSubmit, formState: { errors } } = useForm();
@@ -24,17 +25,104 @@ const SettingsFormNew = () => {
   }
 
   const handleDeleteInput = (i: number) => {
+    console.log("i", i)
     setInputNum(origInput => {
       return origInput.filter(input => input !== i);
     })
   } 
 
-  const onSubmit = (data: any) => {
+  const onSubmit = async (data: any) => {
     // Simulate form submission
-    console.log('Submitted Data:', data);
-    setSubmittedData(data);
+    await saveFeedData(data);
   };
 
+  const saveFeedData = async (rssFeeds: any) => {
+    try {
+      const jsonValue = JSON.stringify(rssFeeds);
+      await AsyncStorage.setItem("rssFeeds", jsonValue);
+      setFeedDataStorage(rssFeeds);
+      alert("Feeds Saved!")
+    } catch (e) {
+      alert("There was a problem saving your RSS Feeds");
+    }
+  };
+
+  const getFeedData = async () => {
+    try {
+      const jsonValue = await AsyncStorage.getItem("rssFeeds");
+      return jsonValue != null ? JSON.parse(jsonValue) : null;
+    } catch (e) {
+      // error reading value
+    }
+  };
+
+  useEffect(() => {
+    (async () => {
+      // Set Feed urls to state.
+      await getFeedData().then((result) => {
+        if (result !== null) {
+          setFeedDataStorage(result);
+        }
+      });
+    })
+  }, []);
+
+  let form: any = <></>
+  if (Object.keys(feedDataStorage).length > 0) {
+    form = Object.keys(feedDataStorage).map((feed: any, i: number) =>(
+      <View key={i+1} style={styles.inputWrapper}>
+        <Controller
+          control={control}
+          render={({ field }) => (
+            <TextInput
+              {...field}
+              style={[
+                theme === 'dark' ? styles.inputDark : styles.inputLight,
+              ]}
+              placeholder="Rss Feed Url"
+              ref={(element) => inputRef.current[i+1] = element}
+              defaultValue={feed[`feed_url_${i+1}`]}
+              value={feed[`feed_url_${i+1}`]}
+            />
+          )}
+          name={`feed_url_${i+1}`}
+          rules={{ required: 'You must enter a valid url' }}
+        />
+        <Pressable onPress={() => handleDeleteInput(i+1)} data-inputnum={i+1}>
+          <AntDesign name="delete" size={24} color={theme === 'dark' ? "white" : "black"} />
+        </Pressable>
+        {errors.name && <Text style={styles.errorText}>{"errors.name.message"}</Text>}        
+      </View>
+    ))
+  }
+  else {
+    form = inputNum.map((_, i: number) => (
+      <View key={i+1} style={styles.inputWrapper}>
+        <Controller
+          control={control}
+          render={({ field }) => (
+            <TextInput
+              {...field}
+              style={[
+                theme === 'dark' ? styles.inputDark : styles.inputLight,
+              ]}
+              placeholder="Rss Feed Url"
+              ref={(element) => inputRef.current[i+1] = element}
+              defaultValue={submittedData[i+1]}
+              value={submittedData[i+1]}
+            />
+          )}
+          name={`feed_url_${i+1}`}
+          rules={{ required: 'You must enter a valid url' }}
+        />
+        <Pressable onPress={() => handleDeleteInput(i+1)} data-inputnum={i+1}>
+          <AntDesign name="delete" size={24} color={theme === 'dark' ? "white" : "black"} />
+        </Pressable>
+        {errors.name && <Text style={styles.errorText}>{"errors.name.message"}</Text>}
+      </View>
+    ))
+  }
+  console.log("feedDataStorage", feedDataStorage)
   return (
     <SafeAreaView>
       <View style={styles.container}>
@@ -43,42 +131,22 @@ const SettingsFormNew = () => {
         ]}>
           Enter RSS feeds for your news:
         </Text>
-        {inputNum.map((_, i: number) => (
-          <View key={i+1} style={styles.inputWrapper}>
-            <Controller
-              control={control}
-              render={({ field }) => (
-                <TextInput
-                  {...field}
-                  style={[
-                    theme === 'dark' ? styles.inputDark : styles.inputLight,
-                  ]}
-                  placeholder="Rss Feed Url"
-                  ref={(element) => inputRef.current[i+1] = element}
-                  defaultValue=""
-                  value={submittedData[i]}
-                />
-              )}
-              name={`feed_url_${i+1}`}
-              rules={{ required: 'You must enter a valid url' }}
-            />
-            <Pressable onPress={() => handleDeleteInput(i+1)} data-inputnum={i+1}>
-              <AntDesign name="delete" size={24} color={theme === 'dark' ? "white" : "black"} />
-            </Pressable>
-            {errors.name && <Text style={styles.errorText}>{"errors.name.message"}</Text>}
-          </View>
-        ))}
+        {form}
         <Pressable style={[
           theme === 'dark' ? styles.buttonDark : styles.buttonLight,
-        ]} onPress={handleAddInput}>
+          ]} onPress={handleAddInput}>
           <Text style={[
           theme === 'dark' ? styles.buttonTextDark : styles.buttonTextLight,
         ]}>Add RSS</Text>
         </Pressable>
-        <Button
-          title='Save Urls'
-          onPress={handleSubmit(onSubmit)}
-        />
+        <Pressable style={[
+          theme === 'dark' ? styles.buttonSaveDark : styles.buttonSaveLight
+          ]} 
+          onPress={handleSubmit(onSubmit)}>
+            <Text style={[
+              theme === 'dark' ? styles.buttonSaveTextDark : styles.buttonSaveTextLight,
+              ]}>Save Urls</Text>
+        </Pressable>
       </View>
     </SafeAreaView>
   )
@@ -173,6 +241,40 @@ const styles = StyleSheet.create({
     letterSpacing: 0.25,
     color: '#000',
   },
+  buttonSaveDark: {
+    marginTop: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 15,
+    elevation: 3,
+    backgroundColor: '#FFF',
+  },
+  buttonSaveTextDark: {
+    color: '#000',
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+  },
+  buttonSaveLight: {
+    marginTop: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 15,
+    elevation: 3,
+    backgroundColor: '#000',
+  },
+  buttonSaveTextLight: {
+    color: '#FFF',
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+  }
 });
 
 export default SettingsFormNew;
