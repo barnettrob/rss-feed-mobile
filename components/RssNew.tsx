10c0532b82b390c2cc84ab1ab0e6d60ce3da0586
barnettrob <barnett.rob@gmail.com>
2024-06-09 12:42:21 -0400

    building out rss feed

--- components/RssNew.tsx
+++ components/RssNew.tsx
@@ -1,8 +1,14 @@
-import React, {useEffect } from 'react';
+import React, {useEffect, useState } from 'react';
 import { StyleSheet, Text } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { XMLParser } from "fast-xml-parser";
+
+interface Feed {
+  url: string;
+}
 
 const RssNew = () => {
+  const [feed, setFeed] = useState<Array<Feed>>([]);
 
   useEffect(() => {
     getFeedData();
@@ -11,12 +17,34 @@ const RssNew = () => {
   const getFeedData = async () => {
     try {
       const jsonValue = await AsyncStorage.getItem("rss-feeds");
-      alert(jsonValue)
-      return jsonValue != null ? JSON.parse(jsonValue) : null;
+      const jsonObject = jsonValue != null ? JSON.parse(jsonValue) : null;
+      if (typeof jsonObject === "object" && 'rss' in jsonObject) {
+        setFeed(jsonObject.rss);
+        
+      }
+      return jsonObject;
     } catch (e) {
       // error reading value
     }
   };
+
+  const fetchFeed = (rss: any) => {
+    const parser = new XMLParser();
+
+    const url = fetch(rss)
+      .then((result) => result.text())
+      .then((data) => {
+        const feedData = parser.parse(data);
+        return feedData.rss.channel["item"];
+      })
+      .then(function (d) {
+        return d;
+      });
+
+    return url;
+  }
+
+  alert(JSON.stringify(feed))
 
   return (
     <Text>RssNew</Text>
